f4129e8fbcbdf475a1153bc01962243dddb756e2
Tesla-Atoz <teslaatozinspires07@gmail.com>
2021-01-28 11:39:09 +0530

    changed static file support

--- simple_social/urls.py
+++ simple_social/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
+        {'document_root': settings.STATICFILES_DIR}),
 ]
